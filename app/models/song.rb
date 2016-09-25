dc053112e9a26078d617e105ed1b8e546f4aff3d
Justin Roberts <justinroberts@pce-ga.com>
2016-09-25 16:39:35 -0400

    songs prettified, hiding some things that aren't ready

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: { scope: :artist, message: "This song has already been added." }
+    validates :artist, presence: true
 end
